b61c557d65fb8b7a479130f4e766329ca2cd7a1a
Stefano <stefano.turcarelli@icloud.com>
2023-11-23 13:01:47 -0600

    Add functions

--- assets/script/Shape.js
+++ assets/script/Shape.js
@@ -1,1 +1,31 @@
+class Shape {
+  _shape;
+  _color;
+
+  constructor(shape, color) {
+    this.shape = shape;
+    this.color = color;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  set color(color) {
+    this._color = color;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  getInfo() {
+    return `${this.color} ${this.name}`;
+  }
+}
+
 export default Shape;
--- assets/script/app.js
+++ assets/script/app.js
@@ -7,6 +7,23 @@ import {
   sleep,
   randomNumber,
   filterArray,
+  create,
 } from "./utils.js";
 
-import { Shape } from "./Shape.js";
+import Shape from "./Shape.js";
+
+const shapesContainer = select(".shapes");
+const createBtn = select(".create");
+const shapeSelector = select(".shape-selector");
+const colorSelector = select(".color-selector");
+
+function createShape() {
+  // Create and append to the grid box
+}
+
+onEvent("click", createBtn, () => {
+  createShape();
+  // Create a Shape object
+  // Save the new object in an array
+  // Add a new shape (div) to the grid
+});
